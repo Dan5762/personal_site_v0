bea102b4becd82b0847ff8b71433ff10f55437cb
Dan5762 <Daniel@alberg-long.co.uk>
2021-07-01 14:31:05 +0100

    Initiali Commit

---
+++ lib/widgets/myappbar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final AppBar appBar;
+
+  const MyAppBar({required this.appBar});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('Daniel Long'),
+      leading: IconButton(
+        icon: Icon(Icons.home),
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, '/');
+        },
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/about');
+          },
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              "About", 
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              )
+            ),
+          ),
+        ),
+        IconButton(
+          icon: FaIcon(FontAwesomeIcons.github),
+          onPressed: () => launch('https://github.com/Dan5762'),
+        ),
+        IconButton(
+          icon: FaIcon(FontAwesomeIcons.linkedin),
+          onPressed: () => launch('https://www.linkedin.com/in/daniel-long-724b21171/'),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => new Size.fromHeight(appBar.preferredSize.height);
+}
